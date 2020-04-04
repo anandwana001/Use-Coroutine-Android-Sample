3bc64a2006c55919bd6c3acc8f9bb6470630a509
anandwana001 <akshaynandwana001@gmail.com>
2020-04-05 01:07:47 +0530

    added example

--- app/src/main/java/com/akshay/usecoroutine/MainActivity.kt
+++ app/src/main/java/com/akshay/usecoroutine/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.akshay.usecoroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+/**
+ * count on background thread,
+ * showing progress on progress bar and
+ * update the UI post completion
+ */
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* GlobalScope.launch {
+             (1..10).forEach { count ->
+                 delay(2000)
+                 // ERROR - Only the original thread that created a view hierarchy can touch its views.
+                 // we tried to access the textView from background thread
+                 textView.text = count.toString()
+             }
+         }*/
+
+        GlobalScope.launch(Dispatchers.Main) {
+            textView.text = "Downloading"
+            progressBar2.visibility = View.VISIBLE
+            calculateProgress(100)
+            textView.text = "Downloaded"
+            progressBar2.visibility = View.GONE
+        }
+    }
+
+    private suspend fun calculateProgress(input: Int) {
+        withContext(Dispatchers.Default) {
+            (0..input step 10).forEach { count ->
+                delay(1000)
+                runOnUiThread {
+                    progressBar.progress = count
+                }
+            }
+        }
     }
 }
